3a830eb789485b0942ef9688e8f0a37a5d471b96
Konrad Dańko <konrad.danko@gmail.com>
2021-01-09 01:57:08 +0100

    Commit of project configuration files

---
+++ src/main/java/pl/coderslab/WarsztatMod6Hibernate/WarsztatMod6HibernateApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.WarsztatMod6Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatMod6HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarsztatMod6HibernateApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/coderslab/WarsztatMod6Hibernate/WarsztatMod6HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.WarsztatMod6Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsztatMod6HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
